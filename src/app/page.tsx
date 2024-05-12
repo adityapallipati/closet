bf0facc87c2c7fb2509ef797477e5a5c105b4306
adityapallipati <adityapallipati@gmail.com>
2024-05-12 16:10:30 -0500

    ui and build updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
+import { mock } from "node:test";
+
+const mockUrls = [
+  "https://utfs.io/f/6669f6a1-d9e7-47b8-85b8-8c22e1a4251b-1u7qm.png",
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
+
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello, Closet App in progres ...</h1>
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))
+      }
+    </div>
+    <h1>Test</h1>
     </main>
   );
 }
