719db53566dfe05917d27c41e568aacdc9db6199
adityapallipati <adityapallipati@gmail.com>
2024-05-12 21:18:23 -0500

    postgres db added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,17 @@
 import Link from "next/link";
-import { mock } from "node:test";
-
-const mockUrls = [
-  "https://utfs.io/f/6669f6a1-d9e7-47b8-85b8-8c22e1a4251b-1u7qm.png",
-]
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}))
+import { db } from "../server/db/index";
+import HomeImage  from "../components/HomeImage";
 
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">{
-        mockImages.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt="image" />
-          </div>
-        ))
-      }
-    </div>
-    <h1>Test</h1>
+    <main className="" >
+      <HomeImage />
     </main>
   );
 }
