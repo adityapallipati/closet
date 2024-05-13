aa1a28b2b5581391f87733333aaac5a1d2b9a26f
adityapallipati <adityapallipati@gmail.com>
2024-05-12 22:08:11 -0500

    update homepage

--- src/components/HomeImage.tsx
+++ src/components/HomeImage.tsx
@@ -9,9 +9,10 @@ export default async function HomeImage() {
                 {[...homeIcon].map((image, index) => (
                     <div key={image.id + "-" + index} className="w-48">
                         <img src={image.url} />
+                        <h1 className="text-center">Welcome</h1>
                     </div>
                 ))}
-                    <h1 className="text-center">Welcome</h1> {/* grab user name and assign after sign in */}
+                     {/* grab user name and assign after sign in */}
                 </div>
             </div>
     );
