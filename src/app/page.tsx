966ded3328dd9e2914ad4e1c6f56be778f026734
adityapallipati <adityapallipati@gmail.com>
2024-05-12 22:51:39 -0500

    troubleshoot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import { db } from "~/server/db";
 import HomeImage  from "../components/HomeImage";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+export default function HomePage() {
 
   return (
     <main className="" >
