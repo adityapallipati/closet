93e67b9ba69c763332d313e7cd91f08d04c0d9d1
adityapallipati <adityapallipati@gmail.com>
2024-05-12 21:22:10 -0500

    add force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { db } from "../server/db/index";
 import HomeImage  from "../components/HomeImage";
 
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
 
